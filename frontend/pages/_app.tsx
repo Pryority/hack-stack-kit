5147bf34898956a1d905bae773f997c5f703f965
pryority <matthewapryor@gmail.com>
2022-11-25 09:46:29 -0500

    remove old css framework

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,19 @@
-import type { AppProps } from 'next/app';
-import { createClient, Provider } from 'urql';
-import { graphExchange } from '@graphprotocol/client-urql';
-import * as GraphClient from '../.graphclient';
+import type { AppProps } from "next/app";
+import { createClient, Provider } from "urql";
+import { graphExchange } from "@graphprotocol/client-urql";
+import * as GraphClient from "../.graphclient";
 import "../styles/globals.css";
 
 const client = createClient({
-  url: 'graphclient://dummy',
+  url: "graphclient://dummy",
   exchanges: [graphExchange(GraphClient)],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <Provider value={client}>
-        <Component {...pageProps} />
-      </Provider>
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
 
